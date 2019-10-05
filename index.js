15ecc1e2118ce0303b4430f62b24ef19c2d82850
ATTILA0516 <a200yen@gmail.com>
2019-10-05 19:05:51 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message =>
+{
+    if (message.channel.name === 'チャンネル名')
+    {
+        if (message.author.bot) return;
+        if (message.attachments.size <= 0)
+        {
+            message.delete()
+        }
+        client.channels.forEach(channel =>
+        {
+            if (message.attachments.size <= 0)
+            {
+                const embed = new Discord.RichEmbed()
+                    .setAuthor(message.author.tag, message.author.avatarURL)
+                    .setDescription(message.content)
+                    .setColor(0x2C2F33)
+                    .setFooter(message.guild.name, message.guild.iconURL)
+                    .setTimestamp()
+                if (channel.name === 'チャンネル名')
+                {
+                    channel.send(embed)
+                    return;
+                }
+                return;
+            }
+            if (!message.attachments.forEach(attachment =>
+            {
+                const embed = new Discord.RichEmbed()
+                    .setAuthor(message.author.tag, message.author.avatarURL)
+                    .setImage(attachment.url)
+                    .setDescription(attachment.url)
+                    .setColor(0x2C2F33)
+                    .setFooter(message.guild.name, message.guild.iconURL)
+                    .setTimestamp()
+                if (channel.name === 'チャンネル名')
+                {
+                    channel.send(embed)
+                    return;
+                }
+                return;
+            }));
+            return;
+        });
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
