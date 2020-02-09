3f0f63557769c3641b1279048c2fa815471d667e
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-10 03:34:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,46 +21,6 @@ client.on('ready', message =>
 
 client.on('message', message =>
 {
-    if (message.content.startsWith('!new')) 
-    {
-        // Fetch all the channels in the guild.
-        let allChannels = message.guild.channels;
-        // Filter out all the non-text channels.
-        let textChannels = allChannels.filter((channel) => 
-        {
-            return channel.type === "text";    
-        });
-        // Filter out all the text channels whose name isn't 'support-(number)'.
-        let supportChannels = textChannels.filter((textChannel) => 
-        {
-            // Checks whether a channel name has format 'support-(number)'. Look into Regex for more info.
-            return textChannel.name.match(/^(support-)\d+$/g);
-        });
-        // Check if there are any support channels.
-        if (supportChannels.length) 
-        {
-            // Get the numbers from the channel name.
-            let numbers = supportChannels.map((supportChannel) => 
-            {
-                return parseInt(supportChannel.name.split('-')[1]);
-            });
-            // Get the highest number from the array.
-            let highestNumber = Math.max(...numbers);
-            // Create a new support channel with the highest number + 1.
-            message.guild.createChannel(`support-${highestNumber+1}`, 'text');
-        } 
-        else 
-        {
-            // There are no support channels, thus create the first one.
-            message.guild.createChannel('support-1', 'text');
-        }
-    }
-    if(message.content.startsWith('addch')) 
-    {
-        var channelName = message.content.replace(/addch/, 'a');
-        message.guild.createChannel(channelName);
-        return;
-    }
     if (message.channel.name === 'スキマ')
     {
         if (message.author.bot) return;
