8d35953c54abd9dfd392566899877cb308928f62
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-03-25 15:26:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.on('message', message =>
         }
         if (talkedRecently.has(message.author.id)) 
         {
-            var msg = message.channel.send("5秒間発言できません。(なお、このメッセージは5秒後にきえます。)-" + message.author)
+            var msg = message.channel.send("5秒間発言できません。-" + message.author)
             function sleep(waitSec, callbackFunc) 
             {
                 var spanedSec = 0;
@@ -88,7 +88,7 @@ client.on('message', message =>
             }
             sleep(5, function() 
             {
-                msg.delete(100);
+                msg.delete();
             })
             return;
         }
